1b436eff651c13eada640ce8ac86b4660102280a
Tom Bielawski <tfbielawski@protonmail.com>
2024-02-26 20:13:03 -0500

    Home component complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 
 function App() {
   return (
-    <div className="text-2xl font-bold">
+    <div className="w-full h-screen text-2xl font-bold">
       <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+        <div className="flex flex-col justify-center items-center w-full h-full">
+
+        </div>
+    </div>
+  )
+}
+
+export default About
